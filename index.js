102c0bbb281d5cfaa63ff0621148e02a4cdfe60b
embuzver8 <41376848+embuzver8@users.noreply.github.com>
2018-07-23 19:23:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TeleBot = require('telebot');
 const bot = new TeleBot('536144761:AAF32-c_LKbUNyK6QAVZhUdG3v1JLxTzSOw');
 
 bot.on('*',async function(msg){
-    bot.sendMessage(msg.chat.id, `Добро пожаловать в бота ${msg.chat.user_name}\n\n\n\n ${msg.text}`)
+    bot.sendMessage(msg.chat.id, `Добро пожаловать в бота хуесос ${msg.chat.user_name}\n\n\n\n ${msg.text}`)
 })
 
 bot.start()
